8755be8419052d192605be3d9075b68e32c335b4
Bhavik Koradiya <silverwebbuzz@gmail.com>
2025-03-25 14:32:15 +0530

    change in folder

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('BASE_PATH', '/home/u843459215/domains/silverwebbuzz.com/public_html/sapi');
 define('BASE_URL', 'https://sapi.silverwebbuzz.com');
-define('BASE_TEMPLATE_URL', 'https://sapi.silverwebbuzz.com/new/');
+define('BASE_TEMPLATE_URL', 'https://sapi.silverwebbuzz.com/template/');
 
 define('PUBLIC_URL', BASE_URL);
 
