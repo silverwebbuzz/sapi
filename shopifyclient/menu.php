bc46764133275e5f748ed216ab709da58c773c82
Bhavik Koradiya <silverwebbuzz@gmail.com>
2025-03-25 18:58:39 +0530

    changes

--- shopifyclient/menu.php
+++ shopifyclient/menu.php
@@ -4,7 +4,7 @@
                 <ul class="menu-inner">
                   <!-- Dashboards -->
                   <li class="menu-item active">
-                    <a href="javascript:void(0)" class="menu-link menu-toggle">
+                    <a href="javascript:void(0)" class="menu-link">
                       <i class="menu-icon icon-base ti tabler-smart-home"></i>
                       <div data-i18n="Dashboards">Dashboards</div>
                     </a>
@@ -36,55 +36,26 @@
                   <li class="menu-item">
                     <a href="javascript:void(0)" class="menu-link menu-toggle">
                       <i class="menu-icon icon-base ti tabler-settings"></i>
-                      <div data-i18n="Pages">Settings</div>
+                      <div data-i18n="Settings">Settings</div>
                     </a>
                     <ul class="menu-sub">
                       
                       <li class="menu-item">
-                        <a href="<?= BASE_SHOPIFY_AF_URL ?>appsetting" class="menu-link menu-toggle">
+                        <a href="<?= BASE_SHOPIFY_AF_URL ?>appsetting" class="menu-link">
                           <i class="menu-icon icon-base ti tabler-tool"></i>
-                          <div data-i18n="Account Settings">Application Settings</div>
+                          <div data-i18n="Application Settings">Application Settings</div>
                         </a>
                       </li>
                       <li class="menu-item">
                         <a href="<?= BASE_SHOPIFY_AF_URL ?>emailsetting" class="menu-link">
                           <i class="menu-icon icon-base ti tabler-mail-cog"></i>
-                          <div data-i18n="FAQ">Email Settings</div>
+                          <div data-i18n="Email Settings">Email Settings</div>
                         </a>
                       </li>
                       <li class="menu-item">
                         <a href="<?= BASE_SHOPIFY_AF_URL ?>invoicesetting" class="menu-link">
                           <i class="menu-icon icon-base ti tabler-file-settings"></i>
-                          <div data-i18n="Pricing">Invoice Settings</div>
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-
-                  <!-- Plan -->
-                  <li class="menu-item">
-                    <a href="javascript:void(0)" class="menu-link menu-toggle">
-                      <i class="menu-icon icon-base ti tabler-settings"></i>
-                      <div data-i18n="Pages">Settings</div>
-                    </a>
-                    <ul class="menu-sub">
-                      
-                      <li class="menu-item">
-                        <a href="<?= BASE_SHOPIFY_AF_URL ?>appsetting" class="menu-link menu-toggle">
-                          <i class="menu-icon icon-base ti tabler-tool"></i>
-                          <div data-i18n="Account Settings">Application Settings</div>
-                        </a>
-                      </li>
-                      <li class="menu-item">
-                        <a href="<?= BASE_SHOPIFY_AF_URL ?>emailsetting" class="menu-link">
-                          <i class="menu-icon icon-base ti tabler-mail-cog"></i>
-                          <div data-i18n="FAQ">Email Settings</div>
-                        </a>
-                      </li>
-                      <li class="menu-item">
-                        <a href="<?= BASE_SHOPIFY_AF_URL ?>invoicesetting" class="menu-link">
-                          <i class="menu-icon icon-base ti tabler-file-settings"></i>
-                          <div data-i18n="Pricing">Invoice Settings</div>
+                          <div data-i18n="Invoice Settings">Invoice Settings</div>
                         </a>
                       </li>
                     </ul>
