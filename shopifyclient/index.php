36443b20d87e4c45e303f09ac8830da10ee49ac3
Bhavik Koradiya <silverwebbuzz@gmail.com>
2025-03-25 13:05:28 +0530

    Change in paths

--- shopifyclient/index.php
+++ shopifyclient/index.php
@@ -1,3 +1,4 @@
+<?php require_once '../config.php'; ?>
 <!doctype html>
 
 <html
@@ -5,7 +6,7 @@
   class="layout-navbar-fixed layout-menu-fixed layout-compact"
   dir="ltr"
   data-skin="default"
-  data-assets-path="../../assets/"
+  data-assets-path="<?= BASE_TEMPLATE_URL ?>assets/"
   data-template="horizontal-menu-template"
   data-bs-theme="light">
   <head>
@@ -19,7 +20,7 @@
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="../../assets/img/favicon/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="<?= BASE_TEMPLATE_URL ?>assets/img/favicon/favicon.ico" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -28,43 +29,43 @@
       href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
       rel="stylesheet" />
 
-    <link rel="stylesheet" href="../../assets/vendor/fonts/iconify-icons.css" />
+    <link rel="stylesheet" href="<?= BASE_TEMPLATE_URL ?>assets/vendor/fonts/iconify-icons.css" />
 
     <!-- Core CSS -->
     <!-- build:css assets/vendor/css/theme.css  -->
 
-    <link rel="stylesheet" href="../../assets/vendor/libs/node-waves/node-waves.css" />
+    <link rel="stylesheet" href="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/node-waves/node-waves.css" />
 
-    <link rel="stylesheet" href="../../assets/vendor/libs/pickr/pickr-themes.css" />
+    <link rel="stylesheet" href="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/pickr/pickr-themes.css" />
 
-    <link rel="stylesheet" href="../../assets/vendor/css/core.css" />
-    <link rel="stylesheet" href="../../assets/css/demo.css" />
+    <link rel="stylesheet" href="<?= BASE_TEMPLATE_URL ?>assets/vendor/css/core.css" />
+    <link rel="stylesheet" href="<?= BASE_TEMPLATE_URL ?>assets/css/demo.css" />
 
     <!-- Vendors CSS -->
 
-    <link rel="stylesheet" href="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
     <!-- endbuild -->
 
-    <link rel="stylesheet" href="../../assets/vendor/libs/apex-charts/apex-charts.css" />
-    <link rel="stylesheet" href="../../assets/vendor/libs/swiper/swiper.css" />
-    <link rel="stylesheet" href="../../assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css" />
-    <link rel="stylesheet" href="../../assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css" />
-    <link rel="stylesheet" href="../../assets/vendor/fonts/flag-icons.css" />
+    <link rel="stylesheet" href="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/apex-charts/apex-charts.css" />
+    <link rel="stylesheet" href="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/swiper/swiper.css" />
+    <link rel="stylesheet" href="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css" />
+    <link rel="stylesheet" href="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css" />
+    <link rel="stylesheet" href="<?= BASE_TEMPLATE_URL ?>assets/vendor/fonts/flag-icons.css" />
 
     <!-- Page CSS -->
-    <link rel="stylesheet" href="../../assets/vendor/css/pages/cards-advance.css" />
+    <link rel="stylesheet" href="<?= BASE_TEMPLATE_URL ?>assets/vendor/css/pages/cards-advance.css" />
 
     <!-- Helpers -->
-    <script src="../../assets/vendor/js/helpers.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/vendor/js/helpers.js"></script>
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
 
     <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
-    <script src="../../assets/vendor/js/template-customizer.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/vendor/js/template-customizer.js"></script>
 
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
 
-    <script src="../../assets/js/config.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/js/config.js"></script>
   </head>
 
   <body>
@@ -341,7 +342,7 @@
                           <div class="d-flex">
                             <div class="flex-shrink-0 me-3">
                               <div class="avatar">
-                                <img src="../../assets/img/avatars/1.png" alt class="rounded-circle" />
+                                <img src="<?= BASE_TEMPLATE_URL ?>assets/img/avatars/1.png" alt class="rounded-circle" />
                               </div>
                             </div>
                             <div class="flex-grow-1">
@@ -385,7 +386,7 @@
                           <div class="d-flex">
                             <div class="flex-shrink-0 me-3">
                               <div class="avatar">
-                                <img src="../../assets/img/avatars/2.png" alt class="rounded-circle" />
+                                <img src="<?= BASE_TEMPLATE_URL ?>assets/img/avatars/2.png" alt class="rounded-circle" />
                               </div>
                             </div>
                             <div class="flex-grow-1">
@@ -431,7 +432,7 @@
                           <div class="d-flex">
                             <div class="flex-shrink-0 me-3">
                               <div class="avatar">
-                                <img src="../../assets/img/avatars/9.png" alt class="rounded-circle" />
+                                <img src="<?= BASE_TEMPLATE_URL ?>assets/img/avatars/9.png" alt class="rounded-circle" />
                               </div>
                             </div>
                             <div class="flex-grow-1">
@@ -479,7 +480,7 @@
                           <div class="d-flex">
                             <div class="flex-shrink-0 me-3">
                               <div class="avatar">
-                                <img src="../../assets/img/avatars/5.png" alt class="rounded-circle" />
+                                <img src="<?= BASE_TEMPLATE_URL ?>assets/img/avatars/5.png" alt class="rounded-circle" />
                               </div>
                             </div>
                             <div class="flex-grow-1">
@@ -501,7 +502,7 @@
                           <div class="d-flex">
                             <div class="flex-shrink-0 me-3">
                               <div class="avatar">
-                                <img src="../../assets/img/avatars/6.png" alt class="rounded-circle" />
+                                <img src="<?= BASE_TEMPLATE_URL ?>assets/img/avatars/6.png" alt class="rounded-circle" />
                               </div>
                             </div>
                             <div class="flex-grow-1">
@@ -565,7 +566,7 @@
                     href="javascript:void(0);"
                     data-bs-toggle="dropdown">
                     <div class="avatar avatar-online">
-                      <img src="../../assets/img/avatars/1.png" alt class="rounded-circle" />
+                      <img src="<?= BASE_TEMPLATE_URL ?>assets/img/avatars/1.png" alt class="rounded-circle" />
                     </div>
                   </a>
                   <ul class="dropdown-menu dropdown-menu-end">
@@ -574,7 +575,7 @@
                         <div class="d-flex align-items-center">
                           <div class="flex-shrink-0 me-2">
                             <div class="avatar avatar-online">
-                              <img src="../../assets/img/avatars/1.png" alt class="rounded-circle" />
+                              <img src="<?= BASE_TEMPLATE_URL ?>assets/img/avatars/1.png" alt class="rounded-circle" />
                             </div>
                           </div>
                           <div class="flex-grow-1">
@@ -1837,7 +1838,7 @@
                             </div>
                             <div class="col-lg-5 col-md-3 col-12 order-1 order-md-2 my-4 my-md-0 text-center">
                               <img
-                                src="../../assets/img/illustrations/card-website-analytics-1.png"
+                                src="<?= BASE_TEMPLATE_URL ?>assets/img/illustrations/card-website-analytics-1.png"
                                 alt="Website Analytics"
                                 height="150"
                                 class="card-website-analytics-img" />
@@ -1882,7 +1883,7 @@
                           </div>
                           <div class="col-lg-5 col-md-3 col-12 order-1 order-md-2 my-4 my-md-0 text-center">
                             <img
-                              src="../../assets/img/illustrations/card-website-analytics-2.png"
+                              src="<?= BASE_TEMPLATE_URL ?>assets/img/illustrations/card-website-analytics-2.png"
                               alt="Website Analytics"
                               height="150"
                               class="card-website-analytics-img" />
@@ -1926,7 +1927,7 @@
                           </div>
                           <div class="col-lg-5 col-md-3 col-12 order-1 order-md-2 my-4 my-md-0 text-center">
                             <img
-                              src="../../assets/img/illustrations/card-website-analytics-3.png"
+                              src="<?= BASE_TEMPLATE_URL ?>assets/img/illustrations/card-website-analytics-3.png"
                               alt="Website Analytics"
                               height="150"
                               class="card-website-analytics-img" />
@@ -2712,36 +2713,36 @@
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/theme.js -->
 
-    <script src="../../assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/jquery/jquery.js"></script>
 
-    <script src="../../assets/vendor/libs/popper/popper.js"></script>
-    <script src="../../assets/vendor/js/bootstrap.js"></script>
-    <script src="../../assets/vendor/libs/node-waves/node-waves.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/popper/popper.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/vendor/js/bootstrap.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/node-waves/node-waves.js"></script>
 
-    <script src="../../assets/vendor/libs/@algolia/autocomplete-js.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/@algolia/autocomplete-js.js"></script>
 
-    <script src="../../assets/vendor/libs/pickr/pickr.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/pickr/pickr.js"></script>
 
-    <script src="../../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
 
-    <script src="../../assets/vendor/libs/hammer/hammer.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/hammer/hammer.js"></script>
 
-    <script src="../../assets/vendor/libs/i18n/i18n.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/i18n/i18n.js"></script>
 
-    <script src="../../assets/vendor/js/menu.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/vendor/js/menu.js"></script>
 
     <!-- endbuild -->
 
     <!-- Vendors JS -->
-    <script src="../../assets/vendor/libs/apex-charts/apexcharts.js"></script>
-    <script src="../../assets/vendor/libs/swiper/swiper.js"></script>
-    <script src="../../assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/apex-charts/apexcharts.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/swiper/swiper.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js"></script>
 
     <!-- Main JS -->
 
-    <script src="../../assets/js/main.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/js/main.js"></script>
 
     <!-- Page JS -->
-    <script src="../../assets/js/dashboards-analytics.js"></script>
+    <script src="<?= BASE_TEMPLATE_URL ?>assets/js/dashboards-analytics.js"></script>
   </body>
 </html>
