210e7e98d3f0001ead08161066efaff62772fa9a
Bhavik Koradiya <silverwebbuzz@gmail.com>
2025-03-26 18:42:29 +0530

    email aded

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -12,6 +12,10 @@ class ComposerStaticInit1ba944b1b2880695113851e981334169
             'Svg\\' => 4,
             'Sabberworm\\CSS\\' => 15,
         ),
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
         'M' => 
         array (
             'Masterminds\\' => 12,
@@ -34,6 +38,10 @@ class ComposerStaticInit1ba944b1b2880695113851e981334169
         'Sabberworm\\CSS\\' => 
         array (
             0 => __DIR__ . '/..' . '/sabberworm/php-css-parser/src',
+        ),
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
         ),
         'Masterminds\\' => 
         array (
