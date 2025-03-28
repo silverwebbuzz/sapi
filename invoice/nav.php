c3e3924cc0502929b7bb35bee38b55ea92b9ae07
Bhavik Koradiya <silverwebbuzz@gmail.com>
2025-03-28 14:00:04 +0530

    changes

--- invoice/nav.php
+++ invoice/nav.php
@@ -3,8 +3,8 @@
         <li class="<?= basename($_SERVER['PHP_SELF']) == 'index.php' ? 'active' : '' ?>">
             <a href="index.php"><i class="icon-dashboard"></i> Dashboard</a>
         </li>
-        <li class="<?= basename($_SERVER['PHP_SELF']) == 'orders.php' ? 'active' : '' ?>">
-            <a href="orders.php"><i class="icon-orders"></i> Orders</a>
+        <li class="<?= basename($_SERVER['PHP_SELF']) == 'order.php' ? 'active' : '' ?>">
+            <a href="orders.php"><i class="icon-orders"></i>Shopify Orders</a>
         </li>
         <li class="menu-dropdown <?= in_array(basename($_SERVER['PHP_SELF']), ['settings.php']) ? 'active' : '' ?>">
             <a href="settings.php"><i class="icon-settings"></i> Settings <i class="icon-chevron"></i></a>
