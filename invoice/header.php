801a7ddf268d41d3fc419887bc8edc1641d41c16
Bhavik Koradiya <silverwebbuzz@gmail.com>
2025-03-28 15:10:43 +0530

    changes

--- invoice/header.php
+++ invoice/header.php
@@ -1,5 +1,11 @@
-<?php require_once '../config.php'; 
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+require_once '../config.php'; 
 require_once '../db.php';
+$conn = DB::getInstance();
 ?>
 <!DOCTYPE html>
 <html lang="en">
