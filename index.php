dff81a9844df09449c1772c27bed56d2feed95d1
Bhavik Koradiya <silverwebbuzz@gmail.com>
2025-04-09 16:50:52 +0530

    changes

--- index.php
+++ index.php
@@ -17,43 +17,31 @@ if (isset($_GET['hmac']) && isset($_GET['shop']) && isset($_GET['timestamp'])) {
 
     $shop = $params['shop'];
 
-    
-    // Check if installed
-    //$conn = DB::getInstance();
-    //$stmt = $conn->prepare("SELECT id,status FROM stores WHERE shop = ?");
-    //$stmt->bind_param("s", $shop);
-    //$stmt->execute();
-    //$result = $stmt->get_result();
-    //$store = $result->fetch_assoc();
-
-    $store = DBHelper::select(
-        "SELECT id, status FROM stores WHERE shop = ? AND status = ?",
-        "si", 
-        [$shop, 'installed']
+    $store = DBHelper::selectOne(
+        "SELECT id, status FROM stores WHERE `shop` = ? AND `status` = ?",
+        "ss", 
+        [$shop, "installed"]
     );
 
     // If store does not exist or was uninstalled, restart installation
-    if (!$store || $store['status'] === 'uninstalled') {
+    if (!$store ) {
         $install_url = getInstallUrl($shop, SHOPIFY_APP_SCOPES, SHOPIFY_APP_URL . '/shopify/callback');
         header("Location: $install_url");
         exit();
     }
 
-    $stmt = $conn->prepare("
-        SELECT p.name AS plan_name, p.price, p.order_limit, s.cancelled_on, st.id as store_id
+
+    $subscription = DBHelper::selectOne("SELECT p.name AS plan_name, p.price, p.order_limit, s.cancelled_on, st.id as store_id
         FROM store_subscriptions s
         JOIN stores st ON s.store_id = st.id
         JOIN plans p ON s.plan_id = p.id
-        WHERE st.shop = ? AND s.status = 'active'
-    ");
-    $stmt->bind_param("s", $shop);
-    $stmt->execute();
-    $result = $stmt->get_result();
+        WHERE st.shop = ? AND s.status = 'active'",
+        "s", 
+        [$shop]
+    );
 
-    if ($result->num_rows > 0) {
-        // Store is subscribed, show plan details
-        $subscription = $result->fetch_assoc();
-       
+    if ($subscription) {
+        
         // After getting $accessToken successfully:
         $cookieData = [
             'shop_id' => $subscription['store_id'],
