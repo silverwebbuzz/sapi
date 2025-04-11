4061d8c1fab70be092b25c607687ccebbf4a6e41
Bhavik Koradiya <silverwebbuzz@gmail.com>
2025-04-11 15:56:17 +0530

    changes

--- invoice/billing.php
+++ invoice/billing.php
@@ -46,8 +46,7 @@ $plans = DBHelper::select("SELECT * FROM `plans` where price != '0.00'  ORDER BY
                         <div class="plan-price">$<?= number_format($plan['price'], 2) ?><span>/month</span></div>
                     </div>
                     
-                    <ul class="<?php if($plan['id']=='2') echo 'plan-card recommended'; else echo 'plan-features'; ?>">
-                    <?php if($plan['id']=='2') echo '<div class="recommended-badge">Recommended</div>'; ?>
+                    <ul class="plan-features">
                         <li><?= $plan['order_limit'] ?> Invoices/month</li>
                         <li><?= $plan['email_limit'] ?> Email Notifications</li>
                         <li>Auto Create and Send Invoices</li>
