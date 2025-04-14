aea6bc0f2ddf6c21c97122089e972d2e2e110659
Bhavik Koradiya <silverwebbuzz@gmail.com>
2025-04-14 13:48:10 +0530

    changes

--- invoice/helper.php
+++ invoice/helper.php
@@ -191,7 +191,7 @@ function sendemail($shop_id,$order_id){
             if($smtp_settings=="")
             {
                 $default_subject = DEFAULT_EMAIL_SUBJECT;
-                echo $default_body = DEFAULT_EMAIL_BODY; exit;
+                $default_body = DEFAULT_EMAIL_BODY;
                 $smtp_settings = [
                     'host' => 'mail.silverwebbuzz.com',
                     'port' => '587',
