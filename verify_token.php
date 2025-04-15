5be3b378abadd3e074c58658c0dd1bcb3c00b90f
Bhavik Koradiya <silverwebbuzz@gmail.com>
2025-04-15 19:16:48 +0530

    changes

--- verify_token.php
+++ verify_token.php
@@ -1,6 +1,4 @@
-<?php ini_set('display_errors', 0);
-error_reporting(0);
-header('Content-Type: application/json');
+<?php 
 require_once 'config/config.php';
 require_once 'vendor/autoload.php';
 use Firebase\JWT\JWT;
