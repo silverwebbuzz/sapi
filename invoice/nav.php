4f2e2fae06acb01f65488a99f3c2d163168bc250
Bhavik Koradiya <silverwebbuzz@gmail.com>
2025-04-16 13:16:20 +0530

    changes

--- invoice/nav.php
+++ invoice/nav.php
@@ -25,11 +25,10 @@
 $sql_settings = "SELECT access_token, store_name,smtp_settings,auto_invoice_customer, auto_invoice_personal, email_invoice, email, invoice_templates_id FROM stores WHERE id = ?";
 $row = DBHelper::selectOne($sql_settings,"s", [$shop_id]);
 
-$webhooks = listWebhooks($shop, $row['access_token']);
+/*$webhooks = listWebhooks($shop, $row['access_token']);
 foreach ($webhooks as $hook) {
     echo "Topic: {$hook['topic']}, URL: {$hook['address']}<br>";
-}
-
+}*/
 
 if($currentPlan['price']>0 && $row['smtp_settings']==''): // You can set this based on your SMTP check logic ?>
 <div id="smtp-warning" class="warning-box">
