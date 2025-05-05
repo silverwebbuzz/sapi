dd7173452625cab5578fca4fd8315191542a382a
Bhavik Koradiya <silverwebbuzz@gmail.com>
2025-05-05 13:15:42 +0530

    changes

--- invoice/footer.php
+++ invoice/footer.php
@@ -3,6 +3,11 @@
 </footer>
 <script> const BASE_URL = "<?= BASE_URL ?>";</script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- Popper.js (required for Bootstrap 4 tooltips) -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<!-- Bootstrap 4 JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
 <script src="../js/script.js"></script>
 </body>
