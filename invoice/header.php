7fdc712dcc8d27f84eb6c579d6888e7cbc303977
Bhavik Koradiya <silverwebbuzz@gmail.com>
2025-05-26 16:37:33 +0530

    changes

--- invoice/header.php
+++ invoice/header.php
@@ -73,7 +73,7 @@ if (!isset($currentPlan) || empty($currentPlan)) {
             };
 
             utils.getSessionToken(app).then((token) => {
-                fetch('../verify_token.php', {
+                fetch('../verify_token', {
                     method: 'POST',
                     headers: {
                         'Authorization': 'Bearer ' + token
